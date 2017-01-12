a647266d0091b59480f8fbcb32e901b3c290e0ac
omarfayad <amory.king2012@gmail.com>
2017-01-12 23:04:23 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,17 +2,17 @@
 #Wiadomości
 $config['Admin_offline'] = 'Aktualnie nie ma żadnego administratora na serwerze'; #Wiadomość jeżeli nie ma żadnego administratora na serwerze
 $config['Admin_online'] = 'Administrator został powiadomiony'; #Wiadomość jeżeli jest chociaż jeden administrator na serwerze
-$config['poke_message'] = 'Ktoś potrzebuje twojej pomocy!'; #Wiadomość powiadomienia administratora
+$config['poke_message'] = 'hi all asd asd dfs sdf!'; #Wiadomość powiadomienia administratora
 
 #Konfiguracja czasów powiadomień
 $config['time'] = 300; #Czas odstępu między kolejnymi powiadomieniami administratora(Poke) wyrażony w sekundach
 $config['time_offline'] = 10; #Czas odsepu międy następnymi powiadomieniami administratora(Poke) jeżeli wszyscy użytkownicy opuścili kanał pomocy wyrażony w sekundach
 
 #Konfiguracja połączenia
-$config['ip'] = 'localhost'; #IP serwera ts3
+$config['ip'] = '79.137.3.71:8110'; #IP serwera ts3
 $config['query_port'] = 10011; #Port Query serwera ts3
 $config['login'] = 'serveradmin'; #Login administratora serwera ts3
-$config['password'] = 'Password'; #Hasło administratora serwera ts3
+$config['password'] = 'xqzfJq6N'; #Hasło administratora serwera ts3
 $config['port'] = 9987; #Port serwera ts3
 
 #Konfiguracja bota
